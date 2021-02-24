04992e05278659a270f7032d89b9d69b8e8cd158
Scott Gruber <scottgruber21@gmail.com>
2021-02-24 08:48:18 -0500

    test

--- src/airtable.js
+++ src/airtable.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import {Link} from 'react-router-dom'
 
-const url='/functions/product'
+const url='/api/product'
 
 const Airtable = () => {
 
