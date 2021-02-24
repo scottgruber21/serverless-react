db42c4d0d2b318f996577e2129716cb35fd5786a
Scott Gruber <scottgruber21@gmail.com>
2021-02-24 08:46:02 -0500

    serverless function fix

--- src/airtable.js
+++ src/airtable.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import {Link} from 'react-router-dom'
 
-const url='/api/product'
+const url='/functions/product'
 
 const Airtable = () => {
 
